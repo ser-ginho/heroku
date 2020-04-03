8fc02c74a1dfacd7e938d52d5d1579e111dc82d2
Serginho <serginhotec@gmail.com>
2020-04-03 20:23:44 -0300

    try catch

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -26,18 +26,22 @@ module.exports = {
     },
     
     async create(request, response) {
-        const {title, description, value} = request.body;
+        try {
+            const {title, description, value} = request.body;
 
-        const ong_id = request.headers.authorization;
+            const ong_id = request.headers.authorization;
 
-        const [id] = await connection('incidents').insert({
-            title,
-            description,
-            value,
-            ong_id
-        });
+            const [id] = await connection('incidents').insert({
+                title,
+                description,
+                value,
+                ong_id
+            });
 
-        return response.json({ id });
+            return response.json({ id });
+        } catch(error) {
+            console.log("Erro ao salvar incidente. " + error);
+        }
     },
 
     async delete(request, response) {
