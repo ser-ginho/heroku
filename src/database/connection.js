e0ac419ece5a4669d44176e621e02353492dbb6c
Serginho <serginhotec@gmail.com>
2020-04-03 11:00:20 -0300

    atualizado valor parametro

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const configuration = require('../../knexfile');
 
 const config = process.env.NODE_ENV == 'test' ? configuration.test : 
-    process.env.NODE_ENV == 'prod' ? configuration.production : configuration.development;
+    process.env.NODE_ENV == 'production' ? configuration.production : configuration.development;
 
 const connection = knex(config);
 
